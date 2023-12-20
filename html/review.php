1fe846043a9dd27213e5528aa4384067f67f73f0
H Awan <hua4@njit.edu>
2023-12-20 17:43:23 -0500

    fixing css for few pages

--- html/review.php
+++ html/review.php
@@ -1,32 +1,103 @@
-<html>
-    <head>
-        <title> Forum </title>
-        <!-- link to css stylesheet, has all the formatting--> 
-		<link rel="stylesheet" href="../css/news.css">
-	
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Reviews</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="Login_v5/vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="Login_v5/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="Login_v5/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="Login_v5/vendor/animate/animate.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="Login_v5/vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="Login_v5/vendor/animsition/css/animsition.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="Login_v5/vendor/select2/select2.min.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="Login_v5/vendor/daterangepicker/daterangepicker.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="css/news.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/util.css">
+	<!--===============================================================================================-->
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> 
+    <!--===============================================================================================-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+</head>
 
 <body onload="loadstuff()">
 	
 	<script><?php session_start(); ?></script>
-	
-    <div class="navigationbar">
-        <a href="index.html">Welcome</a>
-        <a href="steamid.html">SteamID</a>
-        <a href="profile.php"> Steam Profile</a>
-        <a href="news.php">Game News</a>
-        <a class="active" href="forum.php">Forum</a>
 
-        <div class="logout">
-            <form method="POST" action="/html/loginbase.php">
-                <div class="logoutarea">
-                    <input type="submit" name="logoutbutton" class="button" value="Log Out">
-                </div>
-            </form>
-        </div>
-    </div>
+	<nav class="navigationbar fixed-top">
+		<ul class="nav-menu">
+			<li class="nav-item">
+				<a href="index.html" class="nav-link">Welcome</a>
+			</li>
+			<li class="nav-item">
+				<a href="steamid.html" class="nav-link">Steam ID</a>
+			</li>
+			<li class="nav-item">
+				<a href="profile.php" class="nav-link">Steam Profile</a>
+			</li>
+			<li class="nav-item">
+				<a href="news.php" class="nav-link">Game News</a>
+			</li>
+		</ul>
+		<div class="logout">
+			<form method="POST" action="/html/loginbase.php">
+				<div class="logoutarea">
+					<input type="submit" name="logoutbutton" class="button" value="Log Out">
+				</div>
+			</form>
+		</div>
+		<div class="hamburger">
+			<span class="bar"></span>
+			<span class="bar"></span>
+			<span class="bar"></span>
+		</div>
+	</nav>
 
-        <div class = "newsbox">
+	<script>
+		const hamburger = document.querySelector(".hamburger");
+		const navMenu = document.querySelector(".nav-menu");
+		const navLink = document.querySelectorAll(".nav-link");
+
+		hamburger.addEventListener("click", mobileMenu);
+
+		function mobileMenu() {
+			hamburger.classList.toggle("active");
+			navMenu.classList.toggle("active");
+		}
+
+		navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+		function closeMenu() {
+			hamburger.classList.remove("active");
+			navMenu.classList.remove("active");
+		}
+	</script>
+
+
+       	<div class="limiter">
+		<div class="container-login100" style="background-image: url('Login_v5/images/bg-01.jpg');">
+			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+					<span class="login100-form-title p-b-53">
+						Reviews
+					</span>
+
+
             <script>
             	function loadstuff(){
             		<?php
@@ -55,7 +126,7 @@
             			else
             				$positive = 0;
             			
-            			require_once('../src/include/loginbase.inc'); 
+            			require_once('src/include/loginbase.inc'); 
 
                         $client = new rabbitMQClient("testRabbitMQ.ini","databaseServer"); 
                         //session_start();
@@ -79,7 +150,7 @@
                 function getposts(){
                     <?php  
 
-                        require_once('../src/include/loginbase.inc'); 
+                        require_once('src/include/loginbase.inc'); 
 
                         $client = new rabbitMQClient("testRabbitMQ.ini","databaseServer"); 
                         //session_start();
@@ -130,7 +201,7 @@
                 		<label for="pos">I enjoyed this game!</label><br>
                 		<input type="radio" id="neg" name="posneg" value="neg" required>
                 		<label for="neg">I did not enjoy this game!</label><br>
-                		<textarea id="messageField" name="message" form="messageForm" placeholder="Give your honest opinion! Just remember to be respectful." rows="80" cols="5" maxlength="400" required></textarea>
+                		<textarea id="messageField" name="message" form="messageForm" placeholder="Give your honest opinion! Just remember to be respectful." rows="5" cols="25" maxlength="400" required></textarea>
                 		<input type="submit" name="makeReview" value="Say it!">
                 	</form>
                 <?php } }?>
